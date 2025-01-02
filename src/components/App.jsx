1c96e1cfb9a4334051f12d3693b8cd2ae2e212d6
DShort13 <dion.short@outlook.com>
2025-01-02 15:47:27 +0000

    Create handleRegistration method in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,10 +6,24 @@ import Login from "./Login";
 import MyProfile from "./MyProfile";
 import Register from "./Register";
 import ProtectedRoute from "./ProtectedRoute";
+import * as auth from "../utils/auth";
 import "./styles/App.css";
 
 function App() {
   const [isLoggedIn, setIsLoggedin] = useState(false);
+
+  const handleRegistration = ({
+    username,
+    email,
+    password,
+    confirmPassword,
+  }) => {
+    if (password === confirmPassword) {
+      auth.Register(username, password, email).then(() => {
+        // TODO: handle successful registration
+      });
+    }
+  };
 
   return (
     <Routes>
